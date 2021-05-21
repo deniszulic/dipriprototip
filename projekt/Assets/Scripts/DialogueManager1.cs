74d3c1717888662fa1ca8754e14aeb6dd317df2b
deniszulic <35138382+deniszulic@users.noreply.github.com>
2021-05-21 16:47:49 +0200

    dijalog

---
+++ projekt/Assets/Scripts/DialogueManager1.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Pipes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager1 : MonoBehaviour
+{
+    public NPC npc;
+
+    bool isTalking = false;
+
+    float distance;
+    float curResponseTracker = 0;
+
+    public GameObject player;
+    public GameObject dialogueUI;
+
+    public Text npcName;
+    public Text npcDialogueBox;
+    public Text playerResponse;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueUI.SetActive(false);
+    }
+
+    void OnMouseOver()
+    {
+        distance = Vector3.Distance(player.transform.position, this.transform.position);
+        if(distance <= 2.5f)
+        {
+            if(Input.GetKeyDown(KeyCode.E) && isTalking==false)
+            {
+                StartConversation();
+            }
+            else if(Input.GetKeyDown(KeyCode.E) && isTalking == true)
+            {
+                EndDialogue();
+            }
+            
+            if (curResponseTracker == 0 && npc.playerDialogue.Length >= 0)
+            {
+                playerResponse.text = npc.playerDialogue[0];
+            }
+        }
+    }
+
+    void StartConversation()
+    {
+        isTalking = true;
+        curResponseTracker = 0;
+        dialogueUI.SetActive(true);
+        npcName.text = npc.name;
+        npcDialogueBox.text = npc.dialogue[0];
+    }
+
+    void EndDialogue()
+    {
+        isTalking = false;
+        dialogueUI.SetActive(false);
+    }
+    private IEnumerator ActivateOnTimer()
+    {
+        yield return new WaitForSeconds(10f);
+        npcDialogueBox.text = npc.dialogue[1];
+
+    }
+}
